544b74e170ccc442dc9b5c2bf63ee98f3fdb3732
Tushar Jaiswal <72261317+tjaiswal2003@users.noreply.github.com>
2025-05-14 20:06:41 +0530

    Add files via upload

---
+++ 6.Backtracking/NQueensNoOfWays.java
@@ -0,0 +1,63 @@
+public class NQueensNoOfWays {
+        public static boolean isSafe(char board[][],int row,int col){
+            //Vertical up
+            for(int i=row-1;i>=0;i--){
+                if(board[i][col]=='Q'){
+                    return false;
+                }
+            }
+            //diagonal left up
+            for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+                if(board[i][j]=='Q'){
+                    return false;
+                }
+            }
+            //diagonal right up
+            for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++){
+                if(board[i][j]=='Q'){
+                    return false;
+                }
+            }
+            return true;
+        }
+       public static void nQueens(char board[][],int row){
+        //base
+        if (row==board.length) {
+            count++;
+            return;
+        }
+        
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+            nQueens(board, row+1);//function call
+            board[row][j]='.';        //backtracking
+            }
+            
+        }
+       }
+       public static void printBoard(char board[][]){
+        System.out.println("------Chess Board------");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println( );
+        }
+       }
+       static int count=0;
+        public static void main(String[] args) {
+            int n=5;
+            char board[][]=new char[n][n];
+            //initialize
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    board[i][j]='.';
+                }
+            }
+            nQueens(board, 0);
+            System.out.println("Total no. of ways to solve "+n+" Queens="+count);
+        }
+    }
+    
+
